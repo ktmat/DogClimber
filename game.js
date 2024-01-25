c270ad94594fd0248dad491bd39ae8863f63ee10
Kai Matolat <kaimatolat@hotmail.com>
2024-01-25 10:37:18 +1000

    made unbreakable block

--- game.js
+++ game.js
@@ -11,11 +11,14 @@ let gameSplash;
 let brickImage;
 let birdEnemy;
 let snakeEnemy;
+let brickUnbreak;
+let gameEnd;
 
 function preload() {
     gameData = loadJSON("gameElements.json", initialiseGame);
     gameLogo = loadImage("assets/sprites/logo.png");
     brickImage = loadImage("assets/sprites/brick.png");
+    brickUnbreak = loadImage("assets/sprites/brickUnbreak1.png");
     
 }
 
@@ -27,6 +30,7 @@ function setup() {
     startGameButton.mouseClicked(loadLevelOne);
     gameSplash = image(gameLogo, 100, 100);
     world.gravity.y = 5;
+
 }
 
 function draw() {
@@ -35,10 +39,21 @@ function draw() {
     } else if (CURRENTSCREEN == "LEVEL1") {
         loadLevelOne();
         handleInput();
-        camera.x = player.x;
+      //  camera.x = player.x;
         camera.y = player.y;
+        console.log(player.position.y);
+        if (player.position.y <= -900) {
+            for (let i = 0; i < bricks.length; i++) {
+                bricks[i].remove();
+                player.remove();
+                CURRENTSCREEN = "LEVEL2";
+            }
+        }
     } else if (CURRENTSCREEN == "LEVEL2") {
-        levelTwo();
+        loadLevelTwo();
+        handleInput();
+        //  camera.x = player.x;
+        camera.y = player.y;
     } else if (CURRENTSCREEN == "SCOREBOARD") {
         scoreboardScreen();
     }
@@ -70,8 +85,13 @@ function loadLevelOne() {
             for (let j = 0; j < numberOfBricks; j++) {
                 let brickX = (platformData.x / 2) + (j * brickWidth);
                 let brickY = platformData.y;
-                let brick = new Sprite(brickX, brickY, brickWidth, brickHeight, 'static');
-                brick.addImage(brickImage);
+                let brick = createBrick(brickX, brickY, brickWidth, brickHeight, Math.floor(Math.random() * (2 - 1 + 1)) + 1);
+                if (brick.isUnbreakable == 1) {
+                    brick.addImage(brickUnbreak);
+                } else {
+                    brick.addImage(brickImage);
+                }
+               // brick.addImage(brickImage);
                 bricks.push(brick);
             }
         }
@@ -98,8 +118,12 @@ function loadLevelTwo() {
             for (let j = 0; j < numberOfBricks; j++) {
                 let brickX = (platformData.x / 2) + (j * brickWidth);
                 let brickY = platformData.y;
-                let brick = new Sprite(brickX, brickY, brickWidth, brickHeight, 'static');
-                brick.addImage(brickImage);
+                let brick = createBrick(brickX, brickY, brickWidth, brickHeight, Math.floor(Math.random() * (2 - 1 + 1)) + 1);
+                if (brick.isUnbreakable == 1) {
+                    brick.addImage(brickUnbreak);
+                } else {
+                    brick.addImage(brickImage);
+                }
                 bricks.push(brick);
             }
         }
@@ -118,14 +142,26 @@ function handleInput() {
 	} else if (keyIsDown(RIGHT_ARROW)) {
 		player.vel.x = 2.5;
 	} else if (keyIsDown(UP_ARROW)) {
-        player.vel.y -= 1;
+        player.vel.y = -10;
         for (let i = 0; i < bricks.length; i++) {
             if (player.collide(bricks[i])) {
-                // If the player collides with a brick, remove it
-                bricks[i].remove();
+                if (bricks[i].isUnbreakable == 1) {
+
+                } else {
+                    // If the player collides with a brick, remove it
+                    bricks[i].remove();
+                }
             }
         }
     } else {
 		player.vel.x = 0;
 	}
+}
+
+function createBrick(x, y, width, height, isUnbreakable) {
+    let brick = createSprite(x, y, width, height, 'static');
+    brick.isUnbreakable = isUnbreakable;
+    console.log(brick.isUnbreakable);
+
+    return brick;
 }
